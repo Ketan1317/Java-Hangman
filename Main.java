2912331f5d9a174a94600fd37b296f33e1545dd6
Ketan1317 <goyalketan1317@gmail.com>
2025-11-21 23:42:55 +0530

    initial commiot

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = "pizza";
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for(int i=0;i<word.length();i++){
+            wordState.add('_');
+        }
+
+        System.out.println(wordState);
+
+    }
+}
